76559d6509ceb12c0ed18e2431323253b954e73b
Onur Osman Gule <m2.hesaplar@hotmail.com>
2020-01-18 19:10:43 +0300

    Add files via upload

---
+++ CozenAdam_YGS_LYS/Form1.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace CozenAdam_YGS_LYS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.cbKitap = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtSayfa = new System.Windows.Forms.TextBox();
+            this.txtSoru = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnCozum = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.wb1 = new System.Windows.Forms.WebBrowser();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Kitap:";
+            // 
+            // cbKitap
+            // 
+            this.cbKitap.Enabled = false;
+            this.cbKitap.FormattingEnabled = true;
+            this.cbKitap.Items.AddRange(new object[] {
+            "Karekök Matematik YGS"});
+            this.cbKitap.Location = new System.Drawing.Point(12, 48);
+            this.cbKitap.Name = "cbKitap";
+            this.cbKitap.Size = new System.Drawing.Size(221, 24);
+            this.cbKitap.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 79);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Sayfa:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(82, 79);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(42, 17);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Soru:";
+            // 
+            // txtSayfa
+            // 
+            this.txtSayfa.Location = new System.Drawing.Point(15, 100);
+            this.txtSayfa.Name = "txtSayfa";
+            this.txtSayfa.Size = new System.Drawing.Size(45, 22);
+            this.txtSayfa.TabIndex = 4;
+            // 
+            // txtSoru
+            // 
+            this.txtSoru.Location = new System.Drawing.Point(85, 99);
+            this.txtSoru.Name = "txtSoru";
+            this.txtSoru.Size = new System.Drawing.Size(45, 22);
+            this.txtSoru.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(66, 102);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(13, 17);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "-";
+            // 
+            // btnCozum
+            // 
+            this.btnCozum.Location = new System.Drawing.Point(137, 95);
+            this.btnCozum.Name = "btnCozum";
+            this.btnCozum.Size = new System.Drawing.Size(96, 27);
+            this.btnCozum.TabIndex = 7;
+            this.btnCozum.Text = "Çözüm";
+            this.btnCozum.UseVisualStyleBackColor = true;
+            this.btnCozum.Click += new System.EventHandler(this.btnCozum_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::CozenAdam_YGS_LYS.Properties.Resources.CA_logo_64;
+            this.pictureBox1.Location = new System.Drawing.Point(907, 414);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(45, 31);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            // 
+            // wb1
+            // 
+            this.wb1.Location = new System.Drawing.Point(246, 13);
+            this.wb1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.wb1.Name = "wb1";
+            this.wb1.Size = new System.Drawing.Size(706, 395);
+            this.wb1.TabIndex = 9;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(65, 203);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(12, 240);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(221, 196);
+            this.listBox1.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(964, 457);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.wb1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnCozum);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtSoru);
+            this.Controls.Add(this.txtSayfa);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cbKitap);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Çözen Adam YGS LYS Çözümleri";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cbKitap;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtSayfa;
+        private System.Windows.Forms.TextBox txtSoru;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnCozum;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.WebBrowser wb1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ CozenAdam_YGS_LYS/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace CozenAdam_YGS_LYS
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string[,] uye = { {"example@gmail.com","123456"},{"example2@gmail.com","123456"}};
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cbKitap.SelectedIndex = 0;
+            wb1.Navigate("https://www.cozenadam.com/uye/cikis");
+            Thread.Sleep(900); // bunları yükseltirsen garanti olur. İnternet hızına göre.
+            wb1.Navigate("https://www.cozenadam.com/uye/giris");
+            Thread.Sleep(900);
+            
+            
+        }
+
+        private void btnCozum_Click(object sender, EventArgs e)
+        {
+            Giris();
+            
+
+        }
+        void SoruyaGit()
+        {
+            wb1.Navigate("https://www.cozenadam.com/sorucozumu/6-4-" + txtSayfa.Text + "-" + txtSoru.Text);
+        }
+        void Cikis()
+        {
+                wb1.Navigate("https://www.cozenadam.com/uye/cikis");
+                wb1.Navigate("https://www.cozenadam.com/uye/giris");
+        }
+        void OnlyCikis()
+        {
+            wb1.Navigate("https://www.cozenadam.com/uye/cikis");
+        }
+        void OnlyGiris()
+        {
+            wb1.Navigate("https://www.cozenadam.com/uye/giris");
+        }
+        void KalanAl()
+        {
+            HtmlElementCollection el = wb1.Document.GetElementsByTagName("i");
+            foreach (var item in el)
+            {
+                listBox1.Items.Add(item.ToString());
+            }
+           
+        }
+        void Giris()
+        {
+            for (int i = 0; i < (uye.Length/2); i++)
+            {
+                
+                wb1.Navigate("https://www.cozenadam.com/uye/cikis");
+                MessageBox.Show("Giriş Yapılıyor.1..");
+                wb1.Navigate("https://www.cozenadam.com/uye/giris");
+                MessageBox.Show("Giriş Yapılıyor.2..");
+               // MessageBox.Show(wb1.Url.AbsoluteUri);
+                if (wb1.Url.AbsoluteUri == "https://www.cozenadam.com/uye/giris")
+                {
+                    wb1.Document.GetElementById("3gom").Focus();
+                    wb1.Document.GetElementById("3gom").InnerText = uye[i, 0];
+                    wb1.Document.GetElementById("0h4x").Focus();
+                    wb1.Document.GetElementById("0h4x").InnerText = uye[i, 1];
+                    wb1.Document.GetElementById("submit-sign-in").InvokeMember("click");
+                }
+                else { i--; }
+                MessageBox.Show("Giriş Yapılıyor.3..");
+                Thread.Sleep(800);
+                SoruyaGit();
+               // MessageBox.Show(wb1.Url.AbsoluteUri);
+                Thread.Sleep(800);
+                MessageBox.Show("Loading...");
+                if (wb1.Url.AbsoluteUri == "https://www.cozenadam.com/abone" || wb1.Url.AbsoluteUri == "https://www.cozenadam.com/sorucozumu/6-4-" + txtSayfa.Text + "-" + txtSoru.Text) break; // messagebox filan çıkarınca düzeliyor.
+
+                
+
+            } 
+
+            
+        }
+        void sa()
+        {
+            //gereksiz
+            foreach (HtmlElement item in wb1.Document.GetElementsByTagName("i"))
+            {
+                if (item.OuterHtml.Contains("page_item"))
+                {
+                    foreach (HtmlElement childItem in item.Children)
+                    {
+                        if (childItem.TagName == "A")
+                        {
+                            //Click the link/Current element
+                            childItem.InvokeMember("Click");
+                            break;
+                        }
+                    }
+                    break;
+                }
+            } 
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            KalanAl();
+        }
+    }
+}
